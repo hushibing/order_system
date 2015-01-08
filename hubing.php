1e58edbb47c4bcb4c2b3cea9fe44ee3d60520bdc
hubing <hs9800@163.com>
2015-01-08 23:04:13 +0800

    add hubing.php

---
+++ hubing.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bing
+ * Date: 15-1-8
+ * Time: 下午10:59
+ */
+
+class hubing {
+
+    function __constructor()
+    {
+        echo 'asdfe';
+    }
+
+    /**
+     *
+     * @return string
+     */
+    function Add()
+    {
+        $add = 'add file';
+        return $add;
+
+    }
+
+
+} 
